68db1f63123e0419a934dc26f0ef0666b0f2d63b
sarah.xu <sarah.xu@thizgroup.com>
2019-06-12 18:09:43 +0800

    directly show the image in the browser without js participate

--- demo-api/src/main/java/com/example/demo/api/ImageApi.java
+++ demo-api/src/main/java/com/example/demo/api/ImageApi.java
@@ -10,7 +10,9 @@ import lombok.Data;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Decoder;
 
+import javax.servlet.http.HttpServletResponse;
 import java.text.MessageFormat;
 import java.util.Base64;
 
@@ -37,18 +39,21 @@ public interface ImageApi extends RestfulApi{
             this.resourceId = resourceId;
             this.resourceBase64 = MessageFormat.format( "data:{0};base64,{1}" ,mimeType, new String(Base64.getEncoder().encode(resourceDataBytes)));
             this.mimeType=mimeType;
-        }
-    }
-    @ApiOperation("通过RESTful API获取图片")
-    @GetMapping("/{id}")
-    default ResponseBean<ImageResponseBean> getImage(@PathVariable("id") String imageId) {
-        return new ResponseBean<>(new ImageResponseBean(123L,"img/jpg",null));
-    }
 
-    @ApiOperation("预览图片: 上传图片并返回图片的id")
-    @PostMapping("/preview")
-    default ResponseBean<ImageResponseBean> getImage( @RequestPart(required = true) MultipartFile file) {
-        return new ResponseBean<>(new ImageResponseBean(123L,"img/jpg",null));
     }
+    }
+        @ApiOperation("通过RESTful API获取图片")
+        @GetMapping("/base64/{id}")
+        default ResponseBean<ImageResponseBean> getImageAsBase64(@PathVariable("id") String imageId) {
+            return new ResponseBean<>(new ImageResponseBean(123L,"img/jpg",null));
+        }
+
+
+        @ApiOperation("通过RESTful API获取图片，返回流，无需js参与，直接显示图片")
+        @GetMapping("/stream/{id}")
+        default ResponseBean<ImageResponseBean> getImageAsStream(@PathVariable("id") String imageId,HttpServletResponse response) {
+            return new ResponseBean<>(new ImageResponseBean(123L,"img/jpg",null));
+        }
+
 
 }
--- demo-api/src/main/java/com/example/demo/api/impl/ImageApiImpl.java
+++ demo-api/src/main/java/com/example/demo/api/impl/ImageApiImpl.java
@@ -7,21 +7,46 @@ import com.example.demo.service.source.BinaryObjectService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @Slf4j
 public class ImageApiImpl implements ImageApi {
 
   private final BinaryObjectService binaryObjectService;
+
   @Autowired
-  public ImageApiImpl(BinaryObjectService binaryObjectService){
-    this.binaryObjectService=binaryObjectService;
+  public ImageApiImpl(BinaryObjectService binaryObjectService) {
+    this.binaryObjectService = binaryObjectService;
   }
 
+  @Override
+  public ResponseBean<ImageResponseBean> getImageAsBase64(String imageId) {
+    BinaryObjectBean binaryObjectBean = binaryObjectService.getBinaryObject(imageId);
+    return new ResponseBean<>(
+        ImageApi.ImageResponseBean.builder()
+            .mimeType(binaryObjectBean.getMimeType())
+            .resourceDataBytes(binaryObjectBean.getData())
+            .resourceId(binaryObjectBean.getId())
+            .build());
+  }
 
   @Override
-  public ResponseBean<ImageResponseBean> getImage(String imageId) {
+  public ResponseBean<ImageResponseBean> getImageAsStream(
+      @PathVariable("id") String imageId, HttpServletResponse response) {
     BinaryObjectBean binaryObjectBean = binaryObjectService.getBinaryObject(imageId);
-    return new ResponseBean<>(ImageApi.ImageResponseBean.builder().mimeType(binaryObjectBean.getMimeType()).resourceDataBytes(binaryObjectBean.getData()).resourceId(binaryObjectBean.getId()).build());
+    response.setContentType(binaryObjectBean.getMimeType());
+    try (ServletOutputStream out = response.getOutputStream(); ) {
+      out.write(binaryObjectBean.getData());
+      out.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return new ResponseBean<>(new ImageResponseBean(123L, "img/jpg", null));
   }
 }
